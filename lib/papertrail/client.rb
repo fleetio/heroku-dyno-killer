472a13f19cd8764540c3f12f255af8a4ea1e2427
Jorge Valdivia <jvaldivia@rarestep.com>
2016-04-14 03:43:40 -0500

    better search

--- lib/papertrail/client.rb
+++ lib/papertrail/client.rb
@@ -5,7 +5,7 @@ class PapertrailClient
 
   # Read all logs that that output memory
   def events
-    response = RestClient.get 'https://papertrailapp.com/api/v1/events/search.json?q=heroku%2Fweb+sample%23memory_total%3D+OR+r14',
+    response = RestClient.get 'https://papertrailapp.com/api/v1/events/search.json?q=heroku%2Fweb+sample%23memory_total%3D+OR+"error+r14"',
       {"X-Papertrail-Token" => @token}
     data = JSON.parse(response.to_str)
     data["events"]
